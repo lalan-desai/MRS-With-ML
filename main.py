9be5106dfd9323d601e107be064d67857ee11e57
Lalan Desai <lalandesai@outlook.com>
2024-06-11 08:51:17 +0530

    Implemented the logic

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import csv
 import requests
 import movie
+import time
+import threading
 
 def getURL(movieID, apiKEY):
     return 'https://www.omdbapi.com/?i='+ movieID +'&plot=full&apikey=' + apiKEY
 
-# Open CSV file in write mode
-with open('movies_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    fieldnames = ['Title', 'Year', 'Rated', 'Released', 'Runtime', 'Genre', 'Director', 'Writer', 'Actors', 'Plot', 'Language', 'Country', 'Awards', 'Poster', 'imdbRating', 'imdbVotes', 'imdbID']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+def get_movie_data(movieID, apiKEY):
+    request_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+    print(f"{request_time} : Request for movie ID {movieID}")
     
-    # Write header
-    writer.writeheader()
-    
-    # Loop through movie IDs
-    for i in range(1, 6):
-        movieID = 'tt' + str(i).zfill(7)
-        data = requests.get(getURL(movieID, 'e4deafa')).json()
-        movieObject = movie.Movie.from_dict(data)
+    data = requests.get(getURL(movieID, apiKEY)).json()
+    if data.get("Ratings") is not None:
+        del data["Ratings"]  # Exclude Ratings field from the data dictionary
+        return data
+    else:
+        print(f"No ratings found for movie ID {movieID}")
+        return None
+
+def write_movie_data(movieID, apiKEY, writer):
+    row_dict = get_movie_data(movieID, apiKEY)
+    writer.writerow(row_dict)
+
+def main():
+    # Open CSV file in write mode
+    with open('movies_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        fieldnames = ['Title', 'Year', 'Rated', 'Released', 'Runtime', 'Genre', 'Director', 'Writer', 'Actors', 
+                      'Plot', 'Language', 'Country', 'Awards', 'Poster', 'Metascore', 'imdbRating', 'imdbVotes', 
+                      'imdbID', 'Type', 'DVD', 'BoxOffice', 'Production', 'Website', 'Response']
+        
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        
+        threads = []
+        for i in range(1, 1001):
+            movieID = 'tt' + str(i).zfill(7)
+            thread = threading.Thread(target=write_movie_data, args=(movieID, 'e4deafa', writer))
+            thread.start()
+            threads.append(thread)
+        
+        # Wait for all threads to complete
+        for thread in threads:
+            thread.join()
+
+if __name__ == "__main__":
+    main()
