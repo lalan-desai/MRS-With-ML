baa0d55a88f92e2332449f286b38c335612427f3
Lalan Desai <lalandesai@outlook.com>
2024-07-21 16:52:47 +0530

    Added the req,txt file

--- MRS_With_ML/wsgi.py
+++ MRS_With_ML/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MRS_With_ML.settings')
 
 application = get_wsgi_application()
+
+app = application
