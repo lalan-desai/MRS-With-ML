2b18d63735c72d6884a87b42122396e9541254c7
Lalan Desai <lalandesai@outlook.com>
2024-07-21 17:26:50 +0530

    Limit changed

--- MRS_With_ML/views.py
+++ MRS_With_ML/views.py
@@ -17,7 +17,7 @@ def getContentFromIMDBID(ID):
     return result
 
 # Write your SQL query
-query = 'SELECT * FROM utjwurwq_MRS.dashboard_content;'
+query = 'SELECT * FROM utjwurwq_MRS.dashboard_content limit 100;'
 
 # Read data into a DataFrame
 df = pd.read_sql(query, connection)
