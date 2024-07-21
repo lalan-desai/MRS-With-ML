3b3f34576c8ff811d3740b01bf15d946b9bf4c75
Lalan Desai <lalandesai@outlook.com>
2024-07-21 16:47:08 +0530

    Changed Allow Hosts

--- MRS_With_ML/settings.py
+++ MRS_With_ML/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9e=rxn!+#rf2=_gbtgfatj5^@rtcd9gi&lpb!=s!+9nd^#2!a0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
